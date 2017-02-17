29c8408ac2964976cf9b7d1071ef112978f1c0fe
mc007 <asd28071977>
2017-02-17 14:53:52 +0100

    =denodeify

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/denodeify/index.d.ts" />
 /// <reference path="modules/minimatch/index.d.ts" />
 /// <reference path="modules/mkdirp/index.d.ts" />
 /// <reference path="modules/q/index.d.ts" />

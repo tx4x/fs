e2672ef564e16a21df099f0dc43d79829197ad6c
mc007 <asd28071977>
2017-02-20 12:04:24 +0100

    =save only

---
+++ src/imports.ts
@@ -0,0 +1,9 @@
+import * as write_fs from 'write-file-atomic';
+export const file = {
+    write_atomic: write_fs
+};
+export const json = {
+    parse: JSON.parse,
+    serialize: JSON.stringify
+};
+import * as denodeify from 'denodeify';
